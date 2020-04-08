89ab10ff0a2d76a9fc33298bf1f89949358c1d16
alecps <alec@cLabs.co>
2020-04-08 15:06:00 -0400

    fixes blocking channel in ProcessChanges

--- service/monitor/store.go
+++ service/monitor/store.go
@@ -8,11 +8,16 @@ import (
 )
 
 func ProcessChanges(ctx context.Context, changes <-chan *db.BlockChangeSet, dbWriter db.RosettaDBWriter, logger log.Logger) error {
-	for changeSet := range changes {
-		if err := dbWriter.ApplyChanges(ctx, changeSet); err != nil {
-			return err
+	for {
+		select {
+		case changeSet := <-changes:
+			if err := dbWriter.ApplyChanges(ctx, changeSet); err != nil {
+				return err
+			}
+			logger.Info("Stored block changes", "block", changeSet.BlockNumber, "registryUpdates", len(changeSet.RegistryChanges))
+		case <-ctx.Done():
+			return ctx.Err()
 		}
-		logger.Info("Stored block changes", "block", changeSet.BlockNumber, "registryUpdates", len(changeSet.RegistryChanges))
 	}
 	return nil
 }
